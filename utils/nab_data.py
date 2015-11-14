8a7af3ba1574708db24f3c133c19d01403aff0ba
Pedro Tabacof <tabacof@gmail.com>
2015-11-14 13:08:40 -0200

    Adding data loader and time-series plotter

--- utils/nab_data.py
+++ utils/nab_data.py
@@ -1,9 +1,27 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+import os
+
+matplotlib.style.use('ggplot')
 
 class NABData(object):
     def __init__(self):
-        """
-        for loading/plotting NAB data files
-        """
-        pass
+        self.data = {}
+        prepath = ''
+        if os.getcwd().split('/')[-1] == 'utils':
+            prepath = '../'
+        for root, _, files in os.walk(prepath + 'data'):
+            for f in files:
+                if f.endswith('.csv'):
+                   self.data[os.path.splitext(f)[0]] = pd.read_csv(root + '/' + f, index_col = 0, sep = ',') 
+
+    def keys(self):
+        return self.data.keys()
+    
+    def plot(self, key):
+        if type(key) != "str":
+            key = self.keys()[key]
+        self.data[key].plot()
+
