6ce3318dcee5eddef127154e299c8d0e07cdeff1
jstrong <jonathan.strong@gmail.com>
2015-11-14 12:59:28 -0500

    a few enhancements to NABData:  - __getitem__ access (with either string key or integer based as was previously used in plot)  - figsize arg to plot  - added data_dir attr as previous path scheme wasn't working on my system  - title to plot

--- utils/nab_data.py
+++ utils/nab_data.py
@@ -8,6 +8,8 @@ import json
 matplotlib.style.use('ggplot')
 
 class NABData(object):
+    data_dir = os.path.join(os.path.dirname(os.path.abspath('.')), 'data')
+
     def __init__(self):
         prepath = ''
         if os.getcwd().split('/')[-1] == 'utils':
@@ -15,7 +17,7 @@ class NABData(object):
             
         # Get time series data and store it in a dictionary
         self.data = {}
-        for root, _, files in os.walk(prepath + 'data'):
+        for root, _, files in os.walk(self.data_dir):
             for f in files:
                 if f.endswith('.csv'):
                    self.data[os.path.splitext(f)[0]] = pd.read_csv(root + '/' + f, index_col = 0, sep = ',') 
@@ -25,7 +27,7 @@ class NABData(object):
             
         # Get anomaly labels
         self.labels = {}
-        with open(prepath + 'labels/combined_windows.json') as data_file:    
+        with open(os.path.join(self.data_dir, '../labels/combined_windows.json')) as data_file:    
             js = json.load(data_file)
             for k,v in js.iteritems():
                 key = k.split('/')[1].split('.')[0]
@@ -34,12 +36,18 @@ class NABData(object):
                     start = pd.to_datetime(anomalies[0])
                     end = pd.to_datetime(anomalies[1])
                     self.labels[key].append([start, end])
-    
-    def plot(self, key, plot_anomalies = True):
+
+    def __getitem__(self, key):
         if type(key) != "str":
             key = self.data.keys()[key]
-        ax = self.data[key].plot(color = 'b')
+        return self.data[key]
+    
+    def plot(self, key, plot_anomalies = True, figsize=(16, 12)):
+        fig = plt.figure(figsize=figsize)
+        ax = fig.add_subplot(111)
+        self.data[key].plot(color = 'b', ax=ax)
         if plot_anomalies:
             for anomalies in self.labels[key]:
                 ax.axvspan(anomalies[0], anomalies[1], alpha=0.5, facecolor='red')
+        ax.set_title(key)
         plt.show()
