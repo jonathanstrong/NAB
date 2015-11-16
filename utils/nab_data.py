c5b22fd327d2c36ca03ba3933154e9ce67c8adae
Jonathan Strong <jonathan.strong@gmail.com>
2015-11-16 10:24:23 -0500

    summary method to NABData

--- utils/nab_data.py
+++ utils/nab_data.py
@@ -5,6 +5,7 @@ import matplotlib
 import os
 import json
 from collections import OrderedDict
+import natural.date, natural.number
 
 matplotlib.style.use('ggplot')
 
@@ -46,6 +47,10 @@ class NABData(object):
 
     def __getitem__(self, key):
         return self.data[self._get_key(key)]
+
+    def _files_iter(self):
+        for key in self.data.keys():
+            yield key, self.data[key]
     
     def plot(self, key, plot_anomalies = True, figsize=(16, 12)):
         fig = plt.figure(figsize=figsize)
@@ -57,3 +62,31 @@ class NABData(object):
                 ax.axvspan(anomalies[0], anomalies[1], alpha=0.5, facecolor='red')
         ax.set_title(key)
         plt.show()
+
+    def summary(self):
+        "returns a DataFrame with statistics about each file"
+        data = []
+        cols = ['file', 'length', 'features', 'period', 'periods_vary', 'min', 
+                'max', 'mean', 'std', '25th_percentile', '50th_percentile', '75th_percentile']
+        for filename, df in self._files_iter():
+            row = dict([('file', filename)])
+            row['length'] = len(df)
+            row['features'] = len(df.columns)
+            row['period'] = natural.date.compress(df.index[1]-df.index[0])
+            row['periods_vary'] = int(np.unique(np.diff(df.index)).shape == (1,))
+            row['min'] = df['value'].min()
+            row['max'] = df['value'].max()
+            row['mean'] = df['value'].mean()
+            row['std'] = df['value'].std()
+            for q in [25, 50, 75]:
+                row['{}_percentile'.format(natural.number.ordinal(q))] = np.percentile(df['value'], q)
+            data.append(row)
+        return self._round_float_cols(pd.DataFrame(data)[cols])
+
+    @staticmethod
+    def _round_float_cols(df, digits=2):
+        "convenience method; was hard to digest summary table"
+        for col in df.columns: 
+            if df[col].dtype in [np.float64, np.float32]:
+                df[col] = np.round(df[col], digits)
+        return df
